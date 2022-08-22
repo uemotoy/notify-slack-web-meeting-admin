c27cc3eecd45f9e0fe5b019d58437638525ba655
uemoto_y <yusuke.uemoto.j8y@jpgr.denso.com>
2022-08-22 14:15:30 +0900

    API：ユーザー一覧取得を実装

--- api/Users.cs
+++ api/Users.cs
@@ -1,35 +1,83 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
+using NotifySlackWebMeetingsAdmin.Api.Entities;
+using NotifySlackWebMeetingsAdmin.Api.Queries;
+
 namespace NotifySlackWebMeetingAdmin.Api
 {
-    public static class Users
+  public static class Users
+  {
+    [FunctionName("GetUsers")]
+    public static async Task<IActionResult> GetUsers(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "Users")] HttpRequest req,
+        [CosmosDB(
+          databaseName: "notify-slack-web-meeting-db",
+          collectionName: "WebMeetings",
+          ConnectionStringSetting = "CosmosDBConnectionString")]DocumentClient client,
+        ILogger log)
     {
-        [FunctionName("Users")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+      log.LogInformation("C# HTTP trigger function processed a request.");
+      string message = string.Empty;
+
+      try
+      {
+        log.LogInformation("GET SlackChannels");
+
+        // クエリパラメータから検索条件パラメータを設定
+        UsersQueryParameter queryParameter = new UsersQueryParameter()
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+          Ids = req.Query["ids"],
+          Name = req.Query["name"],
+          EmailAddress = req.Query["emailAddress"],
+          UserPrincipal = req.Query["userPrincipal"],
+        };
 
-            string name = req.Query["name"];
+        // Slackチャンネル情報を取得
+        message = JsonConvert.SerializeObject(await GetUsers(client, queryParameter, log));
+      }
+      catch (Exception ex)
+      {
+        return new BadRequestObjectResult(ex);
+      }
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
-        }
+      return new OkObjectResult(message);
     }
+
+
+    internal static async Task<IEnumerable<User>> GetUsers(
+       DocumentClient client,
+       UsersQueryParameter queryParameter,
+       ILogger log
+     )
+    {
+      Uri collectionUri = UriFactory.CreateDocumentCollectionUri("notify-slack-web-meeting-db", "Users");
+      IDocumentQuery<User> query = client.CreateDocumentQuery<User>(collectionUri, new FeedOptions { EnableCrossPartitionQuery = true, PopulateQueryMetrics = true })
+          .Where(queryParameter.GetWhereExpression())
+          .AsDocumentQuery();
+      log.LogInformation(query.ToString());
+
+      var documentItems = new List<User>();
+      while (query.HasMoreResults)
+      {
+        foreach (var documentItem in await query.ExecuteNextAsync<User>())
+        {
+          documentItems.Add(documentItem);
+        }
+      }
+
+      return documentItems;
+    }
+  }
 }
---
+++ api/entities/Users.cs
@@ -0,0 +1,48 @@
+using System;
+using Newtonsoft.Json;
+
+namespace NotifySlackWebMeetingsAdmin.Api.Entities
+{
+  /// <summary>
+  /// Slackチャンネルを表す
+  /// </summary>
+  public class User
+  {
+    /// <summary>
+    /// 既定のコンストラクタ
+    /// </summary>
+    public User()
+    {
+      Id = Guid.NewGuid().ToString();
+    }
+
+    #region プロパティ
+    /// <summary>
+    /// 一意とするID
+    /// </summary>
+    [JsonProperty("id")]
+    public string Id { get; set; }
+    /// <summary>
+    /// ユーザー名
+    /// </summary>
+    [JsonProperty("name")]
+    public string Name { get; set; }
+    /// <summary>
+    /// Eメールアドレス
+    /// </summary>   
+    [JsonProperty("emailAddress")]
+    public string EmailAddress { get; set; }
+    /// <summary>
+    /// ユーザープリンシパル
+    /// </summary>
+    [JsonProperty("userPrincipal")]
+    public string UserPrincipal { get; set; }
+    /// <summary>
+    /// 登録日時（UTC）
+    /// </summary>
+    [JsonProperty("registeredAt")]
+    public DateTime RegisteredAt { get; set; }
+
+    #endregion
+  }
+}
